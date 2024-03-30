f0b6548db4f76adf292514c46a0025e7eed628eb
Alec <alec.p.wills@gmail.com>
2024-03-30 18:45:59 -0400

    2d dm unit test

--- xcquinox/tests/test_xcequinox.py
+++ xcquinox/tests/test_xcequinox.py
@@ -9,7 +9,8 @@ import jax.numpy as jnp
 
 import pytest
 from ase import Atoms
-from pyscfad import dft
+from pyscfad import dft, scf
+from pyscf.dft import UKS as pUKS
 
 import xcquinox as xce
 
@@ -130,5 +131,38 @@ def test_xc_defaults():
     xc = xce.xc.eXC(grid_models = [eX, eC], level=3)
 
     exc = xc(dm, ao_eval, mf.grids.weights)
+    assert exc
+
+def test_xc_spinpol_defaults():
+    h2 = Atoms('HH', positions=[[ 0.      ,  0.      ,  0.371395],
+                                [ 0.      ,  0.      , -0.371395]])
+    
+    name, mol = xce.utils.ase_atoms_to_mol(h2, basis='def2tzvpd')
+    print('Doing short PBE calculation for inputs...')
+    mf = scf.UHF(mol)
+    e_tot = mf.kernel()
+    dm = mf.make_rdm1()
+    mfuks = pUKS(mol, xc='PBE')
+    e_uks = mfuks.kernel()
+
+    ao_eval = jnp.array(mfuks._numint.eval_ao(mol, mfuks.grids.coords, deriv=2))
+
+    eX = xce.net.eX(n_input = 2,
+                n_hidden = 16,
+                depth = 3,
+                use = [1,2],
+                ueg_limit=True,
+                lob = 1.174,
+                seed = 9001)
+    eC = xce.net.eC(n_input = 4,
+                    n_hidden = 16,
+                    depth = 3,
+                    use = [2,3],
+                    ueg_limit=False,
+                    lob = 1.804,
+                    seed = 9001)
+    xc = xce.xc.eXC(grid_models = [eX, eC], level=3)
+
+    exc = xc(dm, ao_eval, mfuks.grids.weights)
     assert exc
 
