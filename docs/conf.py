f914ad76ec64a73ab19483b19a45dfc700059475
Alec <alec.p.wills@gmail.com>
2024-03-30 18:24:00 -0400

    pyscfad, ase to mock imports

--- docs/conf.py
+++ docs/conf.py
@@ -58,7 +58,7 @@ napoleon_google_docstring = False
 napoleon_use_param = False
 napoleon_use_ivar = True
 
-autodoc_mock_imports = ["jax", "equinox"]
+autodoc_mock_imports = ["jax", "equinox", "ase", "pyscfad"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
