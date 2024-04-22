7a41897d84ba90542512cf4342c2e67fcac4a8ca
Alec <alec.p.wills@gmail.com>
2024-04-22 15:58:16 -0400

    added solids testing

--- xcquinox/tests/test_xcquinox_train.py
+++ xcquinox/tests/test_xcquinox_train.py
@@ -73,7 +73,7 @@ def test_train_e_loss():
 
     #trainer to do training
     first_E = xc(dms[0], ao_evals[0], gws[0])
-    xcT = xce.train.xcTrainer(xc, optax.adamw(1e-4), Eloss, steps=10, logfile='test_e_log')
+    xcT = xce.train.xcTrainer(xc, optax.adamw(1e-4), Eloss, steps=3, logfile='test_e_log')
     new_model = xcT(1, xc, dms, energies, ao_evals, gws)
     new_E = new_model(dms[0], ao_evals[0], gws[0])
     assert abs(new_E-energies[0]) < abs(first_E - energies[0])
@@ -116,70 +116,3 @@ def test_train_rho_loss():
                     dmL = 0.0, holuL = 0.0, dm_to_rho=1.0)
 
     assert ( abs(new_loss) < abs(init_loss) )
-
-
-def test_train_bandgap_si():
-    loss = xce.loss.Band_gap_1shot_loss()
-    mfs = []
-    mols = []
-    energies = []
-    dms = []
-    ao_evals = []
-    gws = []
-    eris = []
-    mo_occs = []
-    hcs = []
-    vs = []
-    ts = []
-    ss = []
-    hologaps = []
-    ogds = []
-
-    cell = gtop.Cell()
-    a = 5.43
-    cell.atom = [['Si', [0,0,0]],
-                ['Si', [a/4,a/4,a/4]]]
-    cell.a = jnp.asarray([[0, a/2, a/2],
-                        [a/2, 0, a/2],
-                        [a/2, a/2, 0]])
-    cell.basis = 'gth-szv'
-    cell.pseudo = 'gth-pade'
-    cell.exp_to_discard = 0.1
-    cell.build()
-    #make the kpts, but this is a gamma point calculation
-    kpts = cell.make_kpts([2,2,2])
-    mf = dftp.RKS(cell, xc='pbe0')
-    e = mf.kernel()
-    mfs.append(mf)
-    dm = mf.make_rdm1()
-    dmj = jnp.array(dm)
-    dmj.flags = dm.flags
-    ao_eval = jnp.array(mf._numint.eval_ao(mf.mol, mf.grids.coords, deriv=2))
-    energies.append(jnp.array(mf.get_veff().exc))
-    dms.append(dmj)
-    ogds.append(dm.shape)
-    ao_evals.append(jnp.array(ao_eval))
-    gws.append(jnp.array(mf.grids.weights))
-    ts.append(jnp.array(mf.mol.intor('int1e_kin')))
-    vs.append(jnp.array(mf.mol.intor('int1e_nuc')))
-    mo_occs.append(jnp.array(mf.mo_occ))
-    hcs.append(jnp.array(mf.get_hcore()))
-    eris.append(jnp.array(mf.mol.intor('int2e')))
-    ss.append(jnp.linalg.inv(jnp.linalg.cholesky(mf.mol.intor('int1e_ovlp'))))
-    hologaps.append(jnp.array(mf.mo_energy[mf.mo_occ == 0][0] - mf.mo_energy[mf.mo_occ > 1][-1]))
-
-    #just do regular MGGA optimization, dont use NL for this test
-    xnet = xce.net.eX(n_input = 2, use = [1, 2], ueg_limit=True, lob=1.174)
-    cnet = xce.net.eC(n_input = 4, use = [2, 3], ueg_limit=True)
-    blankxc = xce.xc.eXC(grid_models = [xnet, cnet], level=3)
-    xc = blankxc
-    xct = xce.train.xcTrainer(model=xc, optim=optax.adamw(1e-2), steps=10, loss = loss, do_jit=True, logfile='test_bg_log')
-    newm = xct(1, xct.model, ao_evals, gws, dms, eris, mo_occs, hcs, ss, ogds, [-1.17], mfs)
-    vgf1 = lambda x: xc(x, ao_evals[0], gws[0], mfs[0])
-    vgf2 = lambda x: newm(x, ao_evals[0], gws[0], mfs[0])
-    _, moe1, _ = xce.utils.get_dm_moe(dms[0], eris[0], vgf1, mo_occs[0], hcs[0], ss[0], ogds[0])
-    _, moe2, _ = xce.utils.get_dm_moe(dms[0], eris[0], vgf2, mo_occs[0], hcs[0], ss[0], ogds[0])
-    print(moe1 - moe1[mf.mol.nelectron//2-1])
-    print(moe2 - moe2[mf.mol.nelectron//2-1])
-
-    assert (abs(moe1[0]-(-1.17))) > (abs(moe2[0]-(-1.17)))
