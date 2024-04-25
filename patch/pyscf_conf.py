40d3298a5b400b76713b9af7aab6fb0219c3b1c7
alecpwills <alec.p.wills@gmail.com>
2024-04-25 15:29:50 -0400

    added pyscf conf setup to the ci

---
+++ patch/pyscf_conf.py
@@ -0,0 +1,7 @@
+pyscfad = True
+pyscf_numpy_backend = 'jax'
+pyscf_scipy_linalg_backend = 'pyscfad'
+pyscf_scipy_backend = 'jax'
+# The followings are optional
+pyscfad_scf_implicit_diff = True
+pyscfad_ccsd_implicit_diff = True
