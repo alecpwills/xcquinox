7903f56bd850521d7acdd6bcd8599233de90b646
alecpwills <alec.p.wills@gmail.com>
2024-06-11 15:55:19 -0400

    updates pretrained model file names, dask[distributed] to requirements [no ci]

--- scripts/train_traj.py
+++ scripts/train_traj.py
@@ -4,7 +4,7 @@ from ase.units import Hartree
 from ase.io import read, write
 from time import time
 import numpy as np
-
+import traceback
 #pure pyscf imports
 from pyscf import dft
 from pyscf import gto, scf, cc
@@ -251,6 +251,9 @@ def do_net_calc(epoch, idx,atoms,basis, **kwargs):
         xc_start = time()
         try:
             mf.kernel()
+            e_tot = mf.e_tot
+            dmp = mf.make_rdm1()
+            print('Kernel calculation complete: total energy = {}'.format(e_tot))
             if mf.e_tot >= 0 and kwargs.get('above0mo', False):
                 print('non-negative total energy; trying to rewrite with mo_energy of homo')
                 print(f'mf.e_tot = {mf.e_tot}')
@@ -263,17 +266,17 @@ def do_net_calc(epoch, idx,atoms,basis, **kwargs):
                 print(f'NON-NEGATIVE ENERGY DETECTED.\n{str(atoms.symbols), mol, mf}\nENERGY={mf.e_tot}')
                 raise
             result.calc = SinglePointCalculator(result)
-            result.calc.results = {'energy' : mf.e_tot}
-            result.calc.results = {'dm' : mf.make_rdm1()}
+            result.calc.results = {'energy' : mf.e_tot, 'dm' : mf.make_rdm1()}
             xc_time = time() - xc_start
             with open('timing', 'a') as tfile:
-                tfile.write('{}\t{}\t{}\t{}\n'.format(epoch, idx, atoms.symbols, mf.xc.upper(), xc_time))
+                tfile.write('{}\t{}\t{}\t{}\t{}\n'.format(epoch, idx, atoms.symbols, mf.xc.upper(), xc_time))
             if kwargs['df'] == True:
                 print('Default auxbasis', mf.with_df.auxmol.basis)
             with open('progress','a') as progfile:
-                progfile.write('{}\t{}\t{}\n'.format(epoch, idx, atoms.symbols, result.calc.results['energy']))
+                progfile.write('{}\t{}\t{}\t{}\n'.format(epoch, idx, atoms.symbols, e_tot))
         except Exception as e:
             print(e)
+            print(traceback.format_exc())
             print('Kernel calculation failed, perhaps hydrogen is acting up or there is another issue')
             print('Trying with UHF')
             vgf = lambda x: kwargs['custom_xc_net'](x, mf._numint.eval_ao(mol, mf.grids.coords, deriv=2), mf.grids.weights, mf=mf, coor=mf.grids.coords)
@@ -285,22 +288,20 @@ def do_net_calc(epoch, idx,atoms,basis, **kwargs):
             mf2.network_eval = vgf
             print('Running UHF calculation')
             mf2.kernel()
+            e_tot = mf2.e_tot
+            dmp = mf2.make_rdm1()
             result.calc = SinglePointCalculator(result)
-            result.calc.results = {'energy' : mf2.e_tot}
-            result.calc.results = {'dm' : mf2.make_rdm1()}
+            result.calc.results = {'energy' : mf2.e_tot, 'dm' : mf2.make_rdm1()}
             xc_time = time() - xc_start
             with open('timing', 'a') as tfile:
-                tfile.write('{}\t{}\t{}\t{}\n'.format(epoch, idx, atoms.symbols, 'pyscfad.scf.UHF', xc_time))
+                tfile.write('{}\t{}\t{}\t{}\t{}\n'.format(epoch, idx, atoms.symbols, 'pyscfad.scf.UHF', xc_time))
             if kwargs['df'] == True:
                 print('Default auxbasis', mf.with_df.auxmol.basis)
             with open('progress','a') as progfile:
-                progfile.write('{}\t{}\t{}\n'.format(epoch, idx, atoms.symbols, result.calc.results['energy']))
+                progfile.write('{}\t{}\t{}\t{}\n'.format(epoch, idx, atoms.symbols, e_tot))
 
-    return (result, result.calc.results['energy'], result.calc.results['dm'])
+    return (result, e_tot, dmp)
 
-class E_DM_Loss(eqx.Module):
-
-    def __call__(self, model, refdatatraj, refdatadir)
 
 def loadnet_from_strucdir(path, ninput, use=[]):
     sp = path.split('/')
@@ -363,7 +364,7 @@ def loadnet_from_strucdir(path, ninput, use=[]):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='')
-    parser.add_argument('xyz', action='store', help ='Path to .xyz/.traj file containing list of configurations')
+    # parser.add_argument('xyz', action='store', help ='Path to .xyz/.traj file containing list of configurations')
     parser.add_argument('-charge', '-c', action='store', type=int, help='Net charge of the system', default=0)
     parser.add_argument('-fdf', metavar='fdf', type=str, nargs = '?', default='')
     parser.add_argument('-basis', metavar='basis', type=str, nargs = '?', default='6-311++G(3df,2pd)', help='basis to use. default 6-311++G(3df,2pd)')
@@ -397,8 +398,10 @@ if __name__ == '__main__':
     parser.add_argument('--xc_verbose', default=False, action='store_true', help='If flagged, sets verbosity on the network.')
     #add arguments for training
     parser.add_argument('--n_steps', action='store', type=int, default=200, help='The number training epochs to go through.')
+    parser.add_argument('--singles_start', action='store', type=int, default=4, help='The index at which the single-atom molecules start, assuming they are at the end of a list of trajectories containing molecules and reactions.')
     parser.add_argument('--do_jit', action='store_true', default=False, help='If flagged, will try to utilize JAX-jitting during training')
     parser.add_argument('--train_traj_path', action='store', type=str, default='/home/awills/Documents/Research/xcquinox/scripts/script_data/haunschild_g2/g2_97.traj', help='Location of the data file for use during pre-training')
+    parser.add_argument('--train_data_dir', action='store', type=str, default='/home/awills/Documents/Research/xcquinox/scripts/script_data/haunschild_g2/g2_97.traj', help='Location of the data file for use during pre-training')
     parser.add_argument('--debug', action='store_true', help='If flagged, only selects first in the target list for quick debugging purposes.')
     parser.add_argument('--verbose', action='store_true', help='If flagged, activates verbosity flag in the network.')
     parser.add_argument('--init_lr', action='store', type=float, default=5e-2, help='The initial learning rate for the network.')
@@ -409,7 +412,7 @@ if __name__ == '__main__':
     setattr(__config__, 'cubegen_box_margin', args.cmargin)
     GCHARGE = args.charge
 
-    atoms = read(args.xyz, ':')
+    atoms = read(args.train_traj_path, ':')
     print("==================")
     print("ARGS SUMMARY")
     print(args)
@@ -457,6 +460,53 @@ if __name__ == '__main__':
 
     input_xc = args.xc if not CUSTOM_XC else 'custom_xc'
 
+    class E_DM_Loss(eqx.Module):
+
+        def __call__(self, model, refdatatraj, refdatadir, singles_start):
+            atoms = read(refdatatraj, ':')
+            results = [do_net_calc(0, ia, atoms[ia], basis=args.basis,
+                                            margin=args.cmargin,
+                                            XC=input_xc,
+                                            PBC=args.pbc,
+                                            L=args.L,
+                                            df=args.df,
+                                            pseudo=args.pseudo,
+                                            rerun=args.rerun,
+                                            owcharge=args.overwrite_gcharge,
+                                            restart = args.restart,
+                                            forcepol = False,
+                                            testgen = args.testgen,
+                                            gridlevel = args.mf_grid_level,
+                                            atomize = True,
+                                            custom_xc = CUSTOM_XC,
+                                            custom_xc_net = xcnet) for ia in range(len(atoms)) if ia >= args.startind and ia < args.endind]
+            e_dct = {str(idx)+'_'+str(res[0].symbols): res[1] for idx, res in enumerate(results)}
+            dm_dct = {str(idx)+'_'+str(res[0].symbols): res[1] for idx, res in enumerate(results)}
+            single_e_dct = {k.split('_')[1]:e_dct[k] for k in e_dct.keys() if int(k.split('_')[0]) >= singles_start}
+            ae_e_dct = {str(idx)+'_'+str(res[0].symbols): res[1] for idx, res in enumerate(results) if not res[0].info.get('reaction', None)}
+            rxn_e_dct = {str(idx)+'_'+str(res[0].symbols): res[1] for idx, res in enumerate(results) if res[0].info.get('reaction', None)}
+
+            ae_losses = []
+            dm_losses = []
+            for k, v in ae_e_dct.items():
+                kidx, at = k.split('_')
+                kidx = int(kidx)
+                ATE = v
+                REFATE = atoms[kidx].info['target_energy']
+                syms = atoms[kidx].get_chemical_symbols()
+                print(f'{kidx}_{at} energy : {v}')
+                for sym in syms:
+                    ATE -= single_e_dct[sym]
+                print(f'{kidx}_{at} atomization energy : {ATE}')
+                print(f'{kidx}_{at} reference atomization energy : {REFATE}')
+                ae_losses.append(ATE-REFATE)
+            
+            for k, v in dm_dct.items():
+                refdm = np.load(os.path.join(refdatadir, k+'.dm.npy'))
+                dm_losses.append(jnp.sum((refdm-v)**2))
+                
+            return jnp.sqrt(jnp.mean(jnp.power(ae_losses, 2))) + jnp.sqrt(jnp.mean(jnp.power(dm_losses, 2)))
+
     if not args.rerun:
         print('beginning new progress file')
         with open('progress','w') as progfile:
@@ -473,25 +523,10 @@ if __name__ == '__main__':
         scheduler = optax.exponential_decay(init_value = args.init_lr, transition_begin=args.lr_decay_start, transition_steps=args.n_steps-args.lr_decay_start, decay_rate=args.lr_decay_rate)
         optimizer = optax.adam(learning_rate = scheduler)
 
-        trainer = xce.train.xcTrainer(model=xcnet, optim=optimizer, steps=args.n_steps, loss = PT_E_Loss(), do_jit=args.do_jit, logfile='trlog')
+        trainer = xce.train.xcTrainer(model=xcnet, optim=optimizer, steps=args.n_steps, loss = E_DM_Loss(), do_jit=args.do_jit, logfile='trlog')
 
         cpus = jax.devices(backend='cpu')
-
-        results = [do_net_calc(ia, atoms[ia], basis=args.basis,
-                                        margin=args.cmargin,
-                                        XC=input_xc,
-                                        PBC=args.pbc,
-                                        L=args.L,
-                                        df=args.df,
-                                        pseudo=args.pseudo,
-                                        rerun=args.rerun,
-                                        owcharge=args.overwrite_gcharge,
-                                        restart = args.restart,
-                                        forcepol = args.forcepol,
-                                        testgen = args.testgen,
-                                        gridlevel = args.mf_grid_level,
-                                        atomize = True,
-                                        custom_xc = CUSTOM_XC,
-                                        custom_xc_net = xcnet) for ia in range(len(atoms)) if ia >= args.startind and ia < args.endind]
+        with jax.default_device(cpus[0]):
+            newm = trainer(1, trainer.model, [args.train_traj_path], [args.train_data_dir], [args.singles_start])
 
 # %%
