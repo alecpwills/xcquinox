b232f034120041c49368a9d0ff216e7257b15076
alecpwills <alec.p.wills@gmail.com>
2024-06-18 13:18:02 -0400

    fix sv_calc sleep condition to write memory [no ci]

--- scripts/sv_calculate_traj.py
+++ scripts/sv_calculate_traj.py
@@ -59,5 +59,5 @@ if __name__ == '__main__':
             #wait 2 minutes before rechecking
             print('Process still on-going, sleeping...')
             with open(progfile, 'a') as f:
-                f.write(f'{p.pid}\tONGOING\tSLEEP\n')
+                f.write(f'{p.pid}\tONGOING\tSLEEP\t{psutil.virtual_memory().used/TOTALMEMORY}\n')
             time.sleep(120)
