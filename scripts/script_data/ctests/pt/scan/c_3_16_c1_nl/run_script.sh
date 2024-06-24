4abd8978ac0f60cfdcb167c7112884457828196e
alecpwills <alec.p.wills@gmail.com>
2024-06-24 17:30:11 -0400

    adds pt scan networks in ctests [no ci]

--- scripts/script_data/ctests/pt/scan/c_3_16_c1_nl/run_script.sh
+++ scripts/script_data/ctests/pt/scan/c_3_16_c1_nl/run_script.sh
@@ -1,7 +1,7 @@
-python ~/Documents/Research/xcquinox/scripts/pretrain_exc.py \
+python ~/xcquinox/scripts/pretrain_exc.py \
 --pretrain_level NL \
---pretrain_net_path ~/Documents/Research/xcquinox/scripts/script_data/ctests/ran/c_3_16_c1_nl \
+--pretrain_net_path ~/xcquinox/scripts/script_data/ctests/ran/c_3_16_c1_nl \
 --pretrain_xc SCAN \
 --n_steps 2500 \
---g297_data_path ~/Documents/Research/xcquinox/scripts/script_data/haunschild_g2/g2_97.traj \
+--g297_data_path ~/xcquinox/scripts/script_data/haunschild_g2/g2_97.traj \
 --pretrain_net c
