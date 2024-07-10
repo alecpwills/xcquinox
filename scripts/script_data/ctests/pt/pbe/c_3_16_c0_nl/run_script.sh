41c1be62d78d6ee7eb90d8ab897fe911cc44a51d
alecpwills <alec.p.wills@gmail.com>
2024-07-10 15:37:52 -0400

    adds val_plot.ipynb and the pre-trained PBE networks

---
+++ scripts/script_data/ctests/pt/pbe/c_3_16_c0_nl/run_script.sh
@@ -0,0 +1,7 @@
+python ~/Documents/Research/xcquinox/scripts/pretrain_exc.py \
+--pretrain_level NL \
+--pretrain_net_path ~/Documents/Research/xcquinox/scripts/script_data/ctests/ran/c_3_16_c0_nl \
+--pretrain_xc PBE \
+--n_steps 2500 \
+--g297_data_path ~/Documents/Research/xcquinox/scripts/script_data/haunschild_g2/g2_97.traj \
+--pretrain_net c
