2da9874324855e9fa6b620e404255f7fccf36ea4
Sara Navarro Rodríguez <snavarro@icn2047919344107.icn2.net>
2025-03-27 11:14:49 +0100

    trying to run the notebook v5

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="xcquinox",
+    version="1.0.0",
+    packages=find_packages(),  # Automatically finds packages in your directory
+    install_requires=[
+        # Add any dependencies your package needs here
+    ],
+)
