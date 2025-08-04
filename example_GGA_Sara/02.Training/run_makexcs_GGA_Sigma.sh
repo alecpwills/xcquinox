6410870bca1374b08fb52c9aaeabfb1fd295ac14
Saru local ICN2 <sara.navarro@icn2.cat>
2025-08-04 15:58:14 +0200

    corrected path for cnet

--- example_GGA_Sara/02.Training/run_makexcs_GGA_Sigma.sh
+++ example_GGA_Sara/02.Training/run_makexcs_GGA_Sigma.sh
@@ -1,6 +1,6 @@
 python makexcs_GGA.py \
   --outpath . \
-  --load_xnet_path ../01.Pretraining/GGA_FcNet_sigma_d3_n16_s42_v_10000 \
+  --load_xnet_path ../01.Pretraining/GGA_FxNet_sigma_d3_n16_s42_v_10000 \
   --load_cnet_path ../01.Pretraining/GGA_FcNet_sigma_d3_n16_s42_v_10000 \
   >> run_makexcs_GGA_Sigma.log 2>&1
 echo "Finished running makexcs_GGA.py for GGA Sigma"
