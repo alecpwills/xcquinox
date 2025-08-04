0b1786bbf767ccb8abc62aad2944a36d9cf87cb7
Saru local ICN2 <sara.navarro@icn2.cat>
2025-08-04 15:41:37 +0200

    corrected training path

--- example_GGA_Sara/02.Training/run_train.sh
+++ example_GGA_Sara/02.Training/run_train.sh
@@ -4,7 +4,7 @@
 #SBATCH --nodes=1
 #SBATCH --time=48:00:00
 #SBATCH -p long-40core
-#SBATCH --mail-type=END
+#SBATCH --mail-type=END,FAIL
 #SBATCH --output=OUT_train_%j.out
 #SBATCH --mail-user=Sara.Navarro@stonybrook.edu
 
@@ -15,8 +15,8 @@ source modules.sh
 conda activate xcq
 
 cd /gpfs/scratch/sarnavarro/xcquinox/example_GGA_Sara/02.Training
-python train_traj_Sara_GGA.py --train_traj_path ../../../scripts/script_data/training_subsets/01/subat_ref.traj \
---train_data_dir ../../../scripts/script_data/training_subsets/01 \
+python train_traj_Sara_GGA.py --train_traj_path ../../scripts/script_data/training_subsets/01/subat_ref.traj \
+--train_data_dir ../../scripts/script_data/training_subsets/01 \
 --xc_xc_net_path . \
 --serial --xc_xc_level GGA \
 --mf_grid_level 1 \
